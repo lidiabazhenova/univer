454e250c888a31945ac37059500a74a0c4f55e3f
Lidia <lidiabazhenova@gmail.com>
2018-08-28 08:57:08 +0300

    correction sql

--- src/main/java/com/lidiabazhenova/webapp/sql/init database.sql
+++ src/main/java/com/lidiabazhenova/webapp/sql/init database.sql
@@ -30,11 +30,11 @@ GRANT ALL ON SEQUENCE product_ids TO automation_user;
 GRANT ALL ON TABLE products TO automation_user;
 
 INSERT INTO products (order_id, url, name, price, quantity) VALUES
-  (1, 'https://oz.by/stationery/more10315502.html?sbtoken=9be46ff5c4f4eaf5374409dd55df5112', 'кнопки', 0.12, 3),
-  (2, 'https://oz.by/pencils/more10137784.html?sbtoken=36d7cc45b5fa5da597bf5cbf09fecf2c', 'карандаш', 0.50, 5),
-  (1, 'https://oz.by/pens/more10354332.html?sbtoken=eeb05f9e7b2000e5e21c1d09f42a4f58', 'ручка', 0.65, 20),
-  (2, 'https://oz.by/stationery/more10254731.html?sbtoken=cd9ab238728f6c4773ec8f38f2d62d5f', 'ластик', 0, 0),
-  (1, 'https://oz.by/stationery/more10352701.html?sbtoken=7c11f44037a1590f753b5d8d585d4f3b', 'стержень', 0.02, 100);
+  (1, 'https://oz.by/stationery/more10315502.html', 'кнопки', 0.12, 3),
+  (2, 'https://oz.by/pencils/more10137784.html', 'карандаш', 0.50, 5),
+  (1, 'https://oz.by/pens/more10354332.html', 'ручка', 0.65, 20),
+  (2, 'https://oz.by/stationery/more10254731.html', 'ластик', 0, 0),
+  (1, 'https://oz.by/stationery/more10352701.html', 'стержень', 0.02, 100);
 
 CREATE SEQUENCE user_ids;
 
